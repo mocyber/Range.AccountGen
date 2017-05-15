09d89a270b0f01e388872b46ef7b76454322dfde
Tommy Matthews (theturingnerd) <tommy@huntr.com>
2017-05-15 11:55:12 -0500

    Add project files.

---
+++ Range.AccountGen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range.AccountGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
